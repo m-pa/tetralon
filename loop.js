f5718d1f5ca0e210402fa9e6e7983314d2adc1aa
m-pa <onen7701@gmail.com>
2022-05-26 02:34:13 +0200

    tetrox

---
+++ loop.js
@@ -0,0 +1,213 @@
+/**
+ * Loop.js
+ * A simple animation loop for HTML5 canvas
+ * Author: Sebastian Herrlinger <sebastian@formzoo.com>
+ * Url: sebastian.formzoo.com
+ * License: MIT
+ *
+ * TODO: implement timed property animation
+ *
+ * Copyright 2011 Sebastian Herrlinger
+ */
+
+kk = {}
+
+ kk.Loop = function(context, frameRate)
+{
+   this._context = context;
+   this._ticking = false;
+   this._lastTickTime = 0;
+   this._tickTimeDiff = 0;
+   this._tickTimeout = null;
+   this._startTime = 0;
+   this._frameRate = frameRate || 60;
+   this._frameRateTick = Math.round(1000 / this._frameRate);
+   
+   //Stats
+   this._fps = 0;
+       
+   this._clearContext = function(c){
+     c.clearRect(0, 0, c.canvas.width, c.canvas.height);
+   };
+ 
+   this._switchObject = {
+     showInfo: function(){},
+     tickStart: this._clearContext
+   };
+   
+   //The objects to render
+   this._objects = [];
+   this._removeObjects = [];
+ }
+ 
+ /**
+  * Sets if the frame info is rendered each tick
+  */
+ kk.Loop.prototype.showInfo = function(show, draw){
+   this._switchObject.showInfo = (show) ? this._drawInfo : (typeof(draw) == 'function') ? draw : function(){};
+   return this;
+ };
+ 
+ /**
+  * Sets what happens when we enter a tick
+  * By default the loop clears the canvas context
+  */
+ kk.Loop.prototype.tickStart = function(clear, what){
+   this._switchObject.tickStart = (clear) ? this._clearContext : (typeof(what) == 'function') ? what : function(){};
+   return this;
+ };
+ 
+ /**
+  * Add an Object to the render loop.
+  * An Object has to provide at least the publich methods "draw" and "animate",
+  * and the boolean attributes visible and paused.
+  * A visible == true object will neither be animated nor drawn.
+  * A paused == true object will just be drawn but not animated.
+  * which will be provided with the 2D context,
+  * otherwise it won't be added.
+  */
+ kk.Loop.prototype.push = function(object){
+   if(this._validateObject(object))
+     this._objects.push(object);
+   return this;
+ };
+ 
+ /**
+  * Add an Object to the _front_
+  */
+ kk.Loop.prototype.shift = function(object){
+   if(this._validateObject(object))
+     this._objects.shift(object);
+   return this;
+ };
+ 
+ /**
+  * Add a new object _before_ an existing one
+  */
+ kk.Loop.prototype.addBefore = function(object, before){
+   if(this._validateObject(object) && this._objects.indexOf(before) != -1)
+     this._objects.splice(this._objects.indexOf(before), 0, object);
+   return this;
+ };
+ 
+ /**
+  * Add a new object _after_ an existing one
+  */
+ kk.Loop.prototype.addAfter = function(object, after){
+   if(this._validateObject(object) && this._objects.indexOf(after) != -1)
+     this._objects.splice(this._objects.indexOf(after) + 1, 0, object);
+   return this;
+ };
+ 
+ /**
+  * Remove an Object from the loop
+  */
+ kk.Loop.prototype.remove = function(object){
+   if(this._objects.indexOf(object) != -1){
+     if(!this._ticking){
+       this._remove(object);
+     } else {
+       this._removeObjects.push(object);
+     }
+   }
+   return this;
+ };
+ 
+ /**
+  * Do the removal
+  */
+ kk.Loop.prototype._remove = function(object) {
+   this._objects.splice(this._objects.indexOf(object), 1);
+ };
+ 
+ /**
+  * The main loop
+  */
+ kk.Loop.prototype._tick = function() {
+   if(!this._ticking){
+     this._ticking = true;
+     this._tickTimeDiff = -this._lastTickTime + (this._lastTickTime = Date.now());
+     
+     this._switchObject.tickStart(this._context, this._tickTimeDiff);
+     
+     for(var k = 0; k < this._objects.length; k++) {
+       this._objects[k].tick.call(this._objects[k], this._context, this._tickTimeDiff);
+     }
+     
+     this._fps = Math.round(1000 / this._tickTimeDiff);
+     this._switchObject.showInfo.call(this, this._context, this._tickTimeDiff);
+     
+     this._ticking = false;
+     
+     while(this._removeObjects.length > 0) {
+       this._remove(this._removeObjects.shift());
+     }
+   }
+ };
+ 
+ /**
+  * start the loop
+  */ 
+ kk.Loop.prototype.start = function() {
+   if(this._tickTimeout === null){
+     this._startTime = this._lastTickTime = Date.now();
+     clearInterval(this._tickTimeout);
+     this._tickTimeout = setInterval(function(t){
+       t._tick();
+     }, this._frameRateTick, this);
+   }
+   return this;
+ };
+ 
+ /**
+  * stop the loop
+  */
+ kk.Loop.prototype.stop = function() {
+   clearInterval(this._tickTimeout);
+   this._tickTimeout = null;
+   return this;
+ };
+ 
+ /**
+  * set the max desired framerate for the loop
+  */
+ kk.Loop.prototype.frameRate = function(fps) {
+   this._frameRate = fps;
+   this._frameRateTick = Math.round(1000 / this._frameRate);
+   this.stop();
+   this.start();
+   return this;
+ };
+ 
+ /** 
+  * Access the current FPS
+  */
+ kk.Loop.prototype.fps = function(){
+   return this._fps;
+ };
+ 
+ /** 
+  * How many objects are in the loop
+  */
+ kk.Loop.prototype.size = function(){
+   return this._objects.length;
+ };
+ 
+ /**
+  * Checks if the object we want to add has a tick method we can call in the loop
+  */
+ kk.Loop.prototype._validateObject = function(object) {
+   if(typeof(object.tick) == 'function'){ return true; }
+   else { throw new Error('Objects must provide the public method tick.'); }
+ };
+ 
+ /**
+  * Draws the information string on the canvas if showInfo is true
+  */
+ kk.Loop.prototype._drawInfo = function(context) {
+   context.save();
+   context.font = '800 10px Helvetica, Arial, sans-serif';
+   context.fillStyle = '#009999';
+   context.fillText(this._objects.length + ' Objects ' + this._fps + ' FPS', 5, 10);
+   context.restore();
+ };
---
+++ tetrix.js
@@ -0,0 +1,236 @@
+const canvas = document.querySelector('#tetrix')
+const loop = new kk.Loop(canvas.getContext('2d'))
+
+const boardHeight = 25
+const boardWidth = 15 
+let board = new Array(boardHeight).fill(0).map(() => new Array(boardWidth).fill(0))
+const boardCenter = 7
+const blockSize = 32
+
+const background = {
+  tick(ctx) {
+    ctx.fillStyle = 'fuchsia'
+    ctx.fillRect(0, 0, 480, 800)
+  }
+}
+
+const teePiece = {
+  shapes: [
+    [[0,1],[1,0],[1,1],[1,2]],
+    [[0,1],[1,1],[2,1],[1,2]],
+    [[1,0],[1,1],[1,2],[2,1]],
+    [[1,0],[0,1],[1,1],[2,1]],
+  ],
+  offset: [-1,-1],
+  direction: 0
+}
+
+let currentPiece = Object.assign({
+  position: [0, boardCenter]
+}, teePiece)
+
+const checkCollision = () => {
+  return currentPiece.shapes[currentPiece.direction].some(element => {
+    const y = currentPiece.position[0] + element[0] + currentPiece.offset[0]
+    const x = currentPiece.position[1] + element[1] + currentPiece.offset[1]
+
+    if (y >= boardHeight) {
+      return true
+    }
+
+    if (board[y][x] === 1) {
+      return true
+    }
+    return false
+  })
+} 
+
+const checkBorders = () => {
+  return currentPiece.shapes[currentPiece.direction].some(element => {
+    const x = currentPiece.position[1] + element[1] + currentPiece.offset[1]
+
+    if (x < 0 || x > boardWidth - 1) {
+      return true
+    }
+    return false
+  })
+} 
+
+const renderPieceToBoard = (piece) => {
+  renderCurrentPiece.timeout = 1500
+  let crashed = false
+  piece.shapes[piece.direction].forEach(element => {
+    const y = piece.position[0] + element[0] + piece.offset[0]
+    const x = piece.position[1] + element[1] + piece.offset[1]
+    if (y < 0) {
+      crashed = true
+      return
+    }
+    board[y][x] = 1
+  })
+
+  if (crashed) {
+    gameOver()
+    return
+  }
+
+  board = board.filter(line => !line.every(tile => tile === 1))
+  const linesToAdd = boardHeight - board.length
+  console.log('linesToAdd', linesToAdd, 'height', boardHeight, 'board length', board.length)
+  board.splice(0, 0, ...(new Array(linesToAdd).fill(0).map(() => new Array(boardWidth).fill(0))))
+  console.log(board.length, board[0])
+
+  setTimeout(() => {
+    currentPiece = Object.assign({
+      position: [0, boardCenter]
+    }, teePiece)
+  }, 1500)
+}
+
+const gameOver = () => {
+  console.log('game over')
+}
+
+const renderCurrentPiece = {
+  tick(ctx, timeDiff) {
+    if (!currentPiece) {
+      return
+    }
+
+    if(this.currentTime >= this.timeout) {
+      this.currentTime = 0
+      currentPiece.position[0] += 1
+      
+      if (checkCollision()) {
+        console.log('collsion')
+        currentPiece.position[0] -= 1
+        renderPieceToBoard(currentPiece)
+        currentPiece = null
+        return
+      }
+    }
+
+    currentPiece.shapes[currentPiece.direction].forEach(element => {
+      this.currentTime += timeDiff
+
+      const y = currentPiece.position[0] + element[0] + currentPiece.offset[0]
+      const x = currentPiece.position[1] + element[1] + currentPiece.offset[1]
+
+      ctx.fillStyle = 'blue'
+
+      ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize)
+    })
+  },
+  timeout: 1500,
+  currentTime: 0
+}
+
+const renderBoard = {
+  tick(ctx) {
+    for(let y = 0; y < boardHeight; y++) {
+      for(let x = 0; x < boardWidth; x++) {
+        if (board[y][x] === 1) {
+          ctx.fillStyle = 'blue'
+          ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize)
+        }
+      }
+    }
+  }
+}
+
+const rotateLeft = (piece) => {
+  piece.direction -= 1
+  if (piece.direction < 0) {
+    piece.direction = piece.shapes.length - 1
+  }
+}
+
+const rotateRight = (piece) => {
+  piece.direction += 1
+  if (piece.direction > piece.shapes.length - 1) {
+    piece.direction = 0
+  }
+}
+
+const shiftLeft = (piece) => {
+  piece.position[1] -= 1
+
+  if(checkBorders()) {
+    piece.position[1] += 1
+  }
+}
+
+const shiftRight = (piece) => {
+  piece.position[1] += 1
+
+  if(checkBorders()) {
+    piece.position[1] -= 1
+  }
+}
+
+const keyHandler = (evt) => {
+  console.log(evt.code)
+  switch (evt.code) {
+    case 'KeyA':
+      if (currentPiece) {
+        rotateLeft(currentPiece)
+        
+        if (checkCollision()) {
+          rotateRight(currentPiece)
+          renderPieceToBoard(currentPiece)
+          currentPiece = null
+          return
+        }
+        
+        if (checkBorders()) {
+          rotateRight(currentPiece)
+        }
+      }
+      
+      break;
+    case 'KeyD':
+      if (currentPiece) {
+        rotateRight(currentPiece)
+
+        if (checkCollision()) {
+          rotateLeft(currentPiece)
+          renderPieceToBoard(currentPiece)
+          currentPiece = null
+          return
+        }
+
+        if (checkBorders()) {
+          rotateLeft(currentPiece)
+        }
+      }
+      break;
+
+    case 'ArrowLeft':
+      if (currentPiece) {
+        shiftLeft(currentPiece)
+      }
+      
+      break;
+    case 'ArrowRight':
+      if (currentPiece) {
+        shiftRight(currentPiece)
+      }
+    break;
+
+    case 'ArrowDown':
+      if (currentPiece) {
+        renderCurrentPiece.timeout = 125
+      }
+    break;
+    default:
+      break;
+  }
+
+}
+
+document.addEventListener('keydown', keyHandler);
+
+loop.push(background)
+loop.push(renderCurrentPiece)
+loop.push(renderBoard)
+loop.start()
